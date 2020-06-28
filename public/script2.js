c7ff0bfa9a992f5f8502d8871527595cc7933a91
twinklethecoder <manikamittal2898@gmail.com>
2020-06-29 02:11:45 +0530

    Adding click anywhere button

--- public/script2.js
+++ public/script2.js
@@ -59,7 +59,7 @@ audio[k].onended = function(){
 					   msg.volume=1; // 0 to 1
 					   msg.rate=1;   // 0.1 to 10
 					   msg.pitch=1; // 0 to 2
-					   msg.text="Your total score is"+score_local_val+"Press anywhere to restart the game";
+					   msg.text="Your total score is"+score_local_val+"       Press anywhere to restart the game";
 					   const voice =speaks[0];
 					   console.log("voice detected");
 					   msg.voiceURI=voice.name;
@@ -139,7 +139,7 @@ testSpeech();
 					   msg.volume=1; // 0 to 1
 					   msg.rate=1;   // 0.1 to 10
 					   msg.pitch=1; // 0 to 2
-					   msg.text="Your total score is"+score_local_val+"Press anywhere to restart the game";
+					   msg.text="Your total score is"+score_local_val+"      Press anywhere to restart the game";
 					   const voice =speaks[0];
 					   console.log("voice detected");
 					   msg.voiceURI=voice.name;
